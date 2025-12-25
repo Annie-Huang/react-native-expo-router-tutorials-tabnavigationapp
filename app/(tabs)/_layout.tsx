a6382f520b49a3411958fa154ce38230b851ad90
Annie Huang <annie.hn@gmail.com>
2025-12-25 13:52:40 +1100

    Create app/(tabs)/_layout.tsx file

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Tabs } from 'expo-router';
+import { Text, View } from 'react-native';
+
+const TabsLayout = () => {
+  return <Tabs></Tabs>;
+};
+
+export default TabsLayout;
