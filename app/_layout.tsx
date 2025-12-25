489e054ef050e964cd985a2896afe00e78c90acd
Annie Huang <annie.hn@gmail.com>
2025-12-25 14:36:53 +1100

    Add the <StatusBar style='light' /> into app/_layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,19 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name='(tabs)'
-        options={{ headerShown: false }}
-      ></Stack.Screen>
-    </Stack>
+    <>
+      {/* Adding the Statusbar is claimed to resolve the color contrast issue from the <Tabs> root component in (tabs)\_layout.tsx page in line 10
+          But I cannot tell the different.
+      */}
+      <StatusBar style='light' />
+      <Stack>
+        <Stack.Screen
+          name='(tabs)'
+          options={{ headerShown: false }}
+        ></Stack.Screen>
+      </Stack>
+    </>
   );
 }
